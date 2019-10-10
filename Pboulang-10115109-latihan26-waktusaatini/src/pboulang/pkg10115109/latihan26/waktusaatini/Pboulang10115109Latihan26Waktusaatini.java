6d8f8d495a1ba68244608c7dc1414aba315d8837
IndriYuliyanti <55839650+IndriYuliyanti@users.noreply.github.com>
2019-10-10 20:24:51 +0700

    Add files via upload

---
+++ Pboulang-10115109-latihan26-waktusaatini/src/pboulang/pkg10115109/latihan26/waktusaatini/Pboulang10115109Latihan26Waktusaatini.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulang.pkg10115109.latihan26.waktusaatini;
+
+import java.util.Date;
+
+/**
+ *
+ * @author nama : indri yuliyanti
+ *         nim  : 10115109
+ *          kls : pbo ulang
+ *          tugas ke 2 waktu saat ini
+ */
+public class Pboulang10115109Latihan26Waktusaatini {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Date date = new Date();
+        
+        System.out.println("hari ini adaalah hari :"+date.toString());
+        
+         System.out.println("");
+        System.out.println("  develop BY: indri yuliyanti  ");
+    }
+    
+}
